d13414192336d4cb82580200caf00adf040c82d6
ArturMingl <adiscov90@gmail.com>
2022-12-31 14:49:18 +0300

    Добавил ввод значений с клавиатуры

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] M;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+M = stroka.Split(' ');
+
+void PrintArray(string [] mass)
+{
+    foreach (string el in mass)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+PrintArray(M);
+
+
